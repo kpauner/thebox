198c7488ae99efdcdadfc0e97803dcad32233066
kpauner <kpauner@gmail.com>
2022-11-10 16:04:31 +0100

    for mobile

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var right_control = document.getElementById('right-control');
 
 var left 			= 0;
 var total_slider	= sliders.length;
-var slider_width 	= 1170;
+var slider_width 	= 1170/2;
 var delay			= 5000; //ms
 
 var under_control	= false;
@@ -50,3 +50,41 @@ function setIsUnderControl(){
 }
 
 setInterval(update, delay);
+
+// Form validation
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const usernameSuccess = document.getElementById('name-error');
+const emailSuccess = document.getElementById('email-error');
+const messageSuccess = document.getElementById('message-error');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	validateInput();
+})
+
+function validateInput(){
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const messageValue = message.value.trim();
+
+	if (usernameValue === ''){
+		usernameSuccess.classList.add('error');
+	} else {
+		usernameSuccess.classList.remove('error');
+	}
+	if (emailValue === ''){
+		emailSuccess.classList.add('error');
+	} else {
+		emailSuccess.classList.remove('error');
+	}
+	if (messageValue === ''){
+		messageSuccess.classList.add('error');
+	} else {
+		messageSuccess.classList.remove('error');
+	}
+}
